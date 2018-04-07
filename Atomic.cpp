165ad5d2490932299432be5711a9ade5306070df
Anuj Rai <17442973+anujrai51@users.noreply.github.com>
2018-04-07 15:41:14 +0530

    Accepting inputs in form of printf(x)

--- Atomic.cpp
+++ Atomic.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+//Commands to be entered as printf(x)
 int main()
 {	ofstream filex;
-    unsigned long n,i;
+    unsigned long n,i,l;
     string s,p;
     cin>>n;  //No. of entries
     for(i=0;i<n;i++)
     {
         cin>>p;    //No. entered and stored as single string
-        s+=p;
+        l=p.length();
+        s+=p.substr(7,l-8);
         s+="\n";
     }
 	filex.open("TEXTFILE.txt",ios::app);
